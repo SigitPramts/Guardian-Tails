f492f095edf243ef7710cf3c24a936b1efad33bb
Sigit Pramono <82726325+SigitPramts@users.noreply.github.com>
2024-02-22 17:43:20 +0700

    I'am done !

--- app.py
+++ app.py
@@ -19,6 +19,28 @@ app.config["JWT_SECRET_KEY"] = "rahasia"
 # Menginisialisasi JWTManager dengan aplikasi Flask
 jwt = JWTManager(app)
 
+from flask_swagger_ui import get_swaggerui_blueprint
+
+SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
+API_URL = '/static/openapi.json'  # Our API url (can of course be a local resource)
+
+# Call factory function to create our blueprint
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+    API_URL,
+    config={  # Swagger UI config overrides
+        'app_name': "Test application"
+    },
+    # oauth_config={  # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
+    #    'clientId': "your-client-id",
+    #    'clientSecret': "your-client-secret-if-required",
+    #    'realm': "your-realms",
+    #    'appName': "your-app-name",
+    #    'scopeSeparator': " ",
+    #    'additionalQueryStringParams': {'test': "hello"}
+    # }
+)
+
 # Mengimpor modul-modul yang diperlukan
 import controllers.donatur as donatur
 import controllers.kegiatan as kegiatan
@@ -27,6 +49,7 @@ import controllers.auth as user
 from controllers.auth import ambil_data_user_controller
 import controllers.binatang as binatang
 
+app.register_blueprint(swaggerui_blueprint)
 
 #----------------------------------------------------------------------------
 # Fungsi untuk mendapatkan data pengguna (user)
